6fac1f297d0d3b14e801d79cd5e6a0bb5d6c90fa
blankspacer155 <monnoi.92@gmail.com>
2021-01-19 15:31:44 +0700

    12-1 complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,24 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double data[],int n,double ans[])
+{
+    double sumplus=0,sumpow=0,summul=1,suminv=0,max=data[0],min=data[0];
+    for(int i=0;i<n;i++)
+    {
+        sumpow+=data[i]*data[i];
+        sumplus+=data[i];
+        summul*=data[i];
+        suminv+=1/data[i];
+        if(data[i]>max)max=data[i];
+        if(data[i]<min)min=data[i];
+    }
+   
+     ans[0]=sumplus/n;
+    ans[1]=sqrt((sumpow/n)-pow(((sumplus/n)),2));
+    ans[2]=pow(summul,(double)1/n);
+    ans[3]=n/suminv;
+    ans[4]=max;
+    ans[5]=min; 
+}
